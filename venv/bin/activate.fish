f6459d38f56151dffa266f7ca5a541b9e8203923
Ben Everson <ben.everson@gmail.com>
2014-09-18 20:41:09 -0400

    fixing requirements.txt file

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -32,7 +32,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/bernardeverson/Desktop/park-smart/venv"
+set -gx VIRTUAL_ENV "/Users/beneverson/Desktop/desolate-hamlet-8381/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
