7bb7161932b6a3b1330b41191c06a5ca94860b13
Ben Everson <ben.everson@gmail.com>
2014-10-01 17:48:10 -0400

    added getgoodspots method and mobile support

--- app.py
+++ app.py
@@ -26,5 +26,22 @@ def getviolations():
     # return the data as geojson                                                                  
     return jsonify(data=geojson_violations)
     
+@app.route('/parksmart/api/getgoodspots/', methods=['GET'])
+def getgoodspots():
+    # grab lat, lon, and radius from the request object
+    _lat = request.args.get('lat')
+    _lon = request.args.get('lon')
+    _radius = request.args.get('radius')
+    # query the database
+    _violations = ParkSmart.getGoodSpots(_lat, _lon, _radius)
+     
+    # convert this to geojson
+    geojson_violations = []
+    for doc in _violations:
+        geojson_violations.append(geojson.Feature(geometry=geojson.Point(doc['loc'])))
+        
+    # return the data as geojson                                                                  
+    return jsonify(data=geojson_violations)
+    
 if __name__ == '__main__':
     app.run(debug=True)
