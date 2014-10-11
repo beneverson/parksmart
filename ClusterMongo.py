1d055b822df79127e2ee5565784a223c4d825462
Ben Everson <ben.everson@gmail.com>
2014-10-10 22:59:55 -0400

    re-do of ClusterMongo script to use DBSCAN from scikit-learn and write to mongodb

--- ClusterMongo.py
+++ ClusterMongo.py
@@ -1,71 +1,81 @@
-## cluster the violations in the live mongodb collection
+## cluster all violations, and add to the mongodb instance
 import pymongo
 import os
 from urlparse import urlparse
+import pandas as pd
+import geojson
+from time import strptime, mktime
+from datetime import datetime
+from sklearn.neighbors import DistanceMetric
+from sklearn.cluster import DBSCAN
 
+# get connection to the mongodb instance
 MONGO_URI = 'mongodb://ben:nosreve@ds043210.mongolab.com:43210/heroku_app29696990'
 conn = pymongo.Connection(MONGO_URI)
 db = conn[urlparse(MONGO_URI).path[1:]]
 collection = db.violationhistory
 
-# 'expand cluster' method for dbscan implementation
-def expandCluster(_doc, _neighborDocs, _clusterId, _maxDistance, _minPoints, _precinctID):
-    # add doc to cluster 
-    clustername = str(_clusterId) + "_" + str(_precinctID)
-    collection.update({'_id' : _doc['_id']}, 
-                              {'$set':{'clusterid' : clustername}}, 
-                              upsert=False, multi=False)
-   
-    i = 0 # declare an index into _neighborDocs
-    while _neighborDocs:  # go thru all neighbor docs
-        _neighbor = _neighborDocs.pop(0)
-        # if this document was not visited
-        if str(_neighbor['_id']) not in visitedIds:
-            # mark as visited by adding to the lookup table
-            visitedIds.append(str(_neighbor['_id']))
-            # find documents near '_neighbor' 
-            newNeighborDocs = list(collection.find({'loc' : {'$near' : {'$geometry': _neighbor['loc'], 
-                                                                        '$maxDistance' : _maxDistance} } , 'precinct': _precinctID}))
-            # expand the cluster if we can
-            if len(newNeighborDocs) >= _minPoints:
-                # create a new list that contains only neigbors we haven't seen before
-                differentNeighborDocs = [aDoc for aDoc in newNeighborDocs if not any(str(_d['_id']) == str(aDoc['_id']) for _d in _neighborDocs)]
-                _neighborDocs.extend(differentNeighborDocs) 
-        # if the neigbor has not yet been added to any cluster, add it to the current one
-        if 'clusterid' not in _neighbor:
-            collection.update({'_id' : _neighbor['_id']}, 
-                              {'$set':{'clusterid' : clustername}}, 
-                              upsert=False, multi=False)
-    return
+# open the violations in pandas
+violations = pd.read_csv('csvs/all_violations.csv', index_col=0)
+# group the violations by precinct
+grouped = violations.groupby('precinct')
 
-def DBSCAN_Mongo(_collection, _maxDistance, _minPoints, _precinctID):
-    # init the clusterid
-    clusterID = 0
-    # find each doc in the DB
-    findall_cursor = list(_collection.find()) # find all docs in the db
-    
-    for _doc in findall_cursor:
-        if str(_doc['_id']) not in visitedIds:
-            # mark as visited by adding to the lookup table
-            visitedIds.append(str(_doc['_id']))
-            # find all the points in this area
-            neighborDocs = list(collection.find({'loc' : {'$near' : {'$geometry': _doc['loc'], 
-                                                                        '$maxDistance' : _maxDistance} }, 'precinct': _precinctID}))
-            if len(neighborDocs) < _minPoints:
-                collection.update({'_id' : _doc['_id']}, 
-                              {'$set':{'clusterid' : 'NOISE'}}, 
-                              upsert=False, multi=False)
-            else:
-                clusterID += 1
-                expandCluster(_doc, neighborDocs, clusterID, _maxDistance, _minPoints, _precinctID)
-    
-    return
-    
-# first get a list of the distinct precincts in the collection
-precincts = collection.distinct('precinct')
-for _precinct in precincts:
-    # lookup table for visited docs in the database
-    visitedIds = []
-    print "started cluster of precinct " + str(_precinct)
-    DBSCAN_Mongo(collection, 20, 3,_precinct)
-    print "finished clustering precinct: " + str(_precinct)
+# date formatting method
+def createdate(_violationtime, _violationdate):
+    try: #try to format the date string, if not possible just return None
+         # try to split the date string
+        _m, _d, _y = _violationdate.split('/')
+        # first append an 'M' to the time string
+        _vtime = _violationtime + 'M'
+        if len(_d) < 2:
+            _d = '0' + _d
+        if len(_m) < 2:
+            _m = '0' + _m
+        # combine both strings
+        _dtstr = _vtime + ' ' + _m + ' ' + _d + ' ' + _y
+        # return the corresponding date object
+        _datetimeobj = datetime.fromtimestamp(mktime(strptime(_dtstr, '%H%M%p %m %d %y'))) 
+        return _datetimeobj
+    except ValueError:
+        return None
+
+# the list of violations to ultimately add to mongodb      
+aggregated_list = []
+
+# params for DBSCAN
+_eps = .00025
+_minpts = 3
+# distance metric
+dist = DistanceMetric.get_metric('euclidean')
+
+for name, group in grouped:
+    print "starting cluster of district " + str(name) 
+    # zip the lat lon, into a single vector
+    X = [list(i) for i in zip(group.latitude, group.longitude)]
+    # generate pairwise distance matrix
+    _pairwise = dist.pairwise(X)
+    # generate clusters
+    clusters = DBSCAN(eps=_eps, min_samples=_minpts, metric='precomputed').fit_predict(_pairwise)
+    # save the clustered df
+    clustered_df = group.copy()
+    clustered_df['clusterid'] = clusters
+    # go through the cluster-assigned dataframe and add to 'aggregated_list'
+    for index, row in clustered_df.iterrows():
+        _thedate = createdate(row['violationtime'], row['violationdate'])
+        if _thedate is not None:
+            # create the geojson object
+            geoData = {'type' : 'Point', 'coordinates' : [row['latitude'], row['longitude']]}
+            _violation = {'datetime':_thedate,
+                    'weekday':_thedate.weekday(),
+                    'hour':_thedate.time().hour,
+                    'minute':_thedate.time().minute,
+                    'precinct':row['precinct'],
+                    'loc':geoData,
+                    'clusterid':row['clusterid']}
+            aggregated_list.append(_violation)
+    print "successfully clustered district " + str(name)
+
+print "adding all clustered violations to mongodb"
+# add the entire aggregated list to the mongo instance
+db.violationhistory.insert(aggregated_list)
+print "successfully clustered dataset!"
